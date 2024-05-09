137a5a41bd07a9762dc9522978faa90087b483cc
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2024-05-09 16:15:22 +0700

    ExampleTestInstrumented

--- app/src/androidTest/java/com/mthree/medicalsync/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mthree/medicalsync/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.waseefakhtar.doseapp", appContext.packageName)
+        assertEquals("com.mthree.medicalsync", appContext.packageName)
     }
 }
